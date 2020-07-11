f41e4c28f0b34c8bc3db9e3401cc60055513dccf
alxemade <chaoxu06@126.com>
2020-07-11 20:47:34 +0800

    caffe faster-rcnn python3.6 CPU

--- tools/utils/blob.py
+++ tools/utils/blob.py
@@ -19,7 +19,7 @@ def im_list_to_blob(ims):
     num_images = len(ims)
     blob = np.zeros((num_images, max_shape[0], max_shape[1], 3),
                     dtype=np.float32)
-    for i in range(num_images):
+    for i in xrange(num_images):
         im = ims[i]
         blob[i, 0:im.shape[0], 0:im.shape[1], :] = im
     # Move channels (axis 3) to axis 1
@@ -27,7 +27,6 @@ def im_list_to_blob(ims):
     channel_swap = (0, 3, 1, 2)
     blob = blob.transpose(channel_swap)
     return blob
-
 
 def prep_im_for_blob(im, pixel_means, target_size, max_size):
     """Mean subtract and scale an image for use in a blob."""
